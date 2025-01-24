400a661b4b00bb4d545c146411d4af791746c051
aniket607 <aniketgoyal607@gmail.com>
2025-01-24 17:36:07 +0530

    Add date-fns dependency and refactor fetchEvents to categorize events into upcoming and past

--- actions/fetchEvents.ts
+++ actions/fetchEvents.ts
@@ -1,15 +1,43 @@
 "use server"
 
 import prisma from "@/lib/prisma"
+import { Event, Venue } from "@prisma/client";
+import { startOfDay } from "date-fns";
 
+interface EventWithVenue extends Event {
+  venue: Venue;
+}
+interface EventsResult {
+  upcomingEvents: EventWithVenue[];
+  pastEvents: EventWithVenue[];
+}
 export default async function fetchEvents() {
-    const events = await prisma.event.findMany({
-        include: {
-            venue: true
-        },
-        orderBy: {
-            eventDate: "asc",
-        },
-    })
-    return events
+    const now = new Date();
+    const start = startOfDay(now);
+    try {
+        // Fetch all events in ascending order
+        const events = await prisma.event.findMany({
+          orderBy: {
+            eventDate: "asc", 
+          },
+          include: {
+            venue: true, 
+          }
+        });
+    
+        // Split events into upcoming and past arrays
+        const { upcomingEvents, pastEvents } = events.reduce<EventsResult>((acc, event) => {
+          if (new Date(event.eventDate) > start) {
+            acc.upcomingEvents.push(event);
+          } else {
+            acc.pastEvents.push(event);
+          }
+          return acc;
+        }, { upcomingEvents: [], pastEvents: [] } as EventsResult);
+        return { upcomingEvents, pastEvents };
+      } catch (error) {
+        console.error("[fetchMyEvents] Error fetching events:", error);
+        throw new Error("Failed to fetch events. Please try again later.");
+      }
+
 }
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,7 +3,8 @@ import fetchEvents from "@/actions/fetchEvents";
 import { EventCard } from "@/components/event-card";
 export default async function EventsPage() {
   const events = await fetchEvents()
-  console.log(events)
+  const {pastEvents, upcomingEvents} = events
+
   return (
     <div className="min-h-screen w-full bg-black">
     {/* Stars Background with fixed positioning */}
@@ -22,9 +23,24 @@ export default async function EventsPage() {
           <h2 className="text-4xl font-bold mb-4 text-white">Discover Events</h2>
           <p className="text-xl text-[#9ca3af]">Discover the hottest events in your City</p>
         </div>
+
+        {/* Past Events */}
         <div className="mx-auto">
+          <span className="text-xl font-bold text-white">PAST EVENTS</span>
           <div className="flex flex-col gap-10">
-              {events.map((event) => (
+              {pastEvents.map((event) => (
+                <div key={event.eventId} className="relative">
+                  <EventCard {...event} />
+                </div>
+              ))}
+            </div>
+        </div>
+
+        {/* Upcoming Events */}
+        <div className="mx-auto mt-8">
+          <span className="text-xl font-bold text-white">UPCOMING EVENTS</span>
+          <div className="flex flex-col gap-10">
+              {upcomingEvents.map((event) => (
                 <div key={event.eventId} className="relative">
                   <EventCard {...event} />
                 </div>
